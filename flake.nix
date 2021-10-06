e12489ed246dfb43e553869267c6ad6710a4e548
Peter Esselius <peter.esselius@pagero.com>
2021-10-06 02:10:21 +0200

    match changes upstream

--- flake.nix
+++ flake.nix
@@ -1,21 +1,18 @@
 {
   inputs.flejk.url = "github:esselius/flejk/main";
-  inputs.utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, flejk, utils }@inputs:
-    utils.lib.eachDefaultSystem (system:
-      flejk.lib.evalFlakeModules {
-        inherit system;
-        config = {
-          terraform.stack.stack1 = {
-            global = { };
-            environments = {
-              env1 = { };
-            };
-            layers = {
-              layer1 = { };
-            };
-          };
+  outputs = { self, nixpkgs, flejk }@inputs:
+    flejk.lib.evalModule { inherit inputs; } {
+      terraform.stack.stack1 = {
+        global = { };
+        environments = {
+          env1 = { };
         };
-      });
+        layers = {
+          layer1 = { };
+        };
+      };
+
+      outputs.overlay = a: b: { };
+    };
 }
